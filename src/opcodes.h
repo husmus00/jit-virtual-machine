393056e661aa84da20f7c30263dffe0034c37ba1
husmus00 <60274772+husmus00@users.noreply.github.com>
2025-11-22 01:12:54 +0000

    Add support for JMP instruction

---
+++ src/opcodes.h
@@ -0,0 +1,15 @@
+#ifndef OPCODES_H
+#define OPCODES_H
+
+#define OP_HLT 0x00
+#define OP_ADD 0x01 
+#define OP_SUB 0x02
+#define OP_PSH 0x03 // Push
+#define OP_POP 0x04 // Pop
+#define OP_DUP 0x05 // Duplicate
+#define OP_PRT 0x06 // Print
+#define OP_STR 0x07 // Store
+#define OP_LOD 0x08 // Load
+#define OP_JMP 0x09 // Jump
+
+#endif // OPCODES_H
